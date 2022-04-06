1f6bad1ce81b250190fc959d72ee2a95fba7005b
Jesus Salvador Castillo Javier <jesus.desarrollo@outlook.com>
2022-04-06 12:18:32 -0500

    Se agrego el Proyecto CoreEscuela

---
+++ CoreEscuela/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+        public string ceo="Freddy Vega";
+
+        public void Timbrar()
+        {
+            Console.Beep(2000,3000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre="UBAM";
+            miEscuela.direccion="Teoloyucan Edo. México";
+            miEscuela.añoFundacion=1978;
+            Console.WriteLine("Timbrando");
+            miEscuela.Timbrar();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
